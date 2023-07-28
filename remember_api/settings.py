d274f94cebfd5e1f00215dd45ea3b90c9c9988ba
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-07-28 15:51:36 +0000

    02, added jwt auth

--- remember_api/settings.py
+++ remember_api/settings.py
@@ -37,6 +37,7 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
+# Changes the look of the URL to api view
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
@@ -51,6 +52,12 @@ ALLOWED_HOSTS = [
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 # Application definition
 
