5c422746bfdb064a5d598766281ff804f4f0b9df
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-08-04 12:40:52 +0000

    added allauth and dj_rest_auth

--- app_task/views.py
+++ app_task/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from remember_api.permissions import IsOwnerOrReadOnly
 
 class TasksList(generics.ListAPIView):
 
     # This gets the serialiser information to display 
     serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     # Query is set to all
     queryset = Task.objects.all()
